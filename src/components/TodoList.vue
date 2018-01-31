97c97e38696c5ade2da5c041e1275a2310adcfce
yangs <yangs@gisuni.com>
2018-01-31 14:20:39 +0800

    Delete a Todo by emiting a delete-todo event and corresponding todo to root component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,9 @@
     <div>
       <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
       <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-      <todo v-for="todo in todos" v-bind:todo="todo" :key="todo.id"></todo>
+      <todo
+        v-on:delete-todo="deleteTodo" v-for="todo in todos" v-bind:todo="todo" :key="todo.id">
+      </todo>
     </div>
 </template>
 
@@ -15,6 +17,12 @@ export default {
   props: ['todos'],
   components: {
     Todo,
+  },
+  methods: {
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
   },
 };
 </script>
