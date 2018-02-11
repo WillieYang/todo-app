fc2c2c98d3a653c6199631144a41c482d2ff5cc1
yangs <yangs@gisuni.com>
2018-02-11 10:22:17 +0800

    Create a store folder to contain the store(state management) file

--- src/components/productList/ProductList.vue
+++ src/components/productList/ProductList.vue
@@ -17,12 +17,6 @@ export default {
   name: 'product-list',
   data() {
     return {
-      products: [
-        { name: 'Banana Skin', price: 20 },
-        { name: 'Shiny Star', price: 40 },
-        { name: 'Green Shells', price: 60 },
-        { name: 'Red Shells', price: 80 },
-      ],
     };
   },
 };
