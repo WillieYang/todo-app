d85f3355f62fb0614c9bdeef34b7471b250a4a97
yangs <yangs@gisuni.com>
2018-02-28 14:17:16 +0800

    Make use of transition component in todo app

--- src/components/todo/CreateTodo.vue
+++ src/components/todo/CreateTodo.vue
@@ -3,28 +3,30 @@
     <button class="ui basic button icon" v-on:click="openForm" v-show="!isCreating">
       <i class="plus icon"></i>
     </button>
-    <div class="ui centered card" v-show="isCreating">
-      <div class="content">
-        <div class="ui form">
-          <div class="field">
-            <label>Title</label>
-            <input v-model="titleText" type="text" ref="title" defaultValue=""/>
-          </div>
-          <div class="field">
-            <label>Project</label>
-            <input v-model="projectText" type="text" ref="project" defaultValue=""/>
-          </div>
-          <div class="ui two button attached buttons">
-            <button class="ui basic blue button" v-on:click="sendForm()">
-              Create
-            </button>
-            <button class="ui basic red button" v-on:click="closeForm()">
-              Cancel
-            </button>
+    <transition name="fade">
+      <div class="ui centered card" v-show="isCreating">
+        <div class="content">
+          <div class="ui form">
+            <div class="field">
+              <label>Title</label>
+              <input v-model="titleText" type="text" ref="title" defaultValue=""/>
+            </div>
+            <div class="field">
+              <label>Project</label>
+              <input v-model="projectText" type="text" ref="project" defaultValue=""/>
+            </div>
+            <div class="ui two button attached buttons">
+              <button class="ui basic blue button" v-on:click="sendForm()">
+                Create
+              </button>
+              <button class="ui basic red button" v-on:click="closeForm()">
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -63,5 +65,21 @@ export default {
 </script>
 
 <style scoped>
-
+.fade-enter-active {
+  animation: fade-in .5s;
+}
+.fade-leave-active {
+  animation: fade-in .5s reverse;
+}
+@keyframes fade-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 </style>
