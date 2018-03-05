f58bdd33a8165ff290f1f69354ffa447164dccc2
yangs <yangs@gisuni.com>
2018-03-05 21:16:16 +0800

    Create a Form by using Bootstrap-vue(with multi-steps), and regard it as a preparation for vuelidate

--- src/components/Authentication/SignUp.vue
+++ src/components/Authentication/SignUp.vue
@@ -3,32 +3,84 @@
     <b-row>
       <b-col></b-col>
       <b-col>
-        <h2>Step 1</h2>
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-          <b-form-group id="firstName"
-                        label="First Name:" class="text-left"
-                        label-for="firstNameLabel"
-                        description="Please Input Your First Name">
-            <b-form-input id="inputFirstName"
-                          type="text"
-                          v-model="userInfo.firstName"
-                          required
-                          placeholder="Input FirstName">
-            </b-form-input>
-          </b-form-group>
-          <b-form-group id="lastName"
-                        label="Last Name:" class="text-left"
-                        label-for="lastNameLabel"
-                        description="Please Input Your Last Name">
-            <b-form-input id="inputLastName"
-                          type="text"
-                          v-model="userInfo.lastName"
-                          required
-                          placeholder="Input LastName">
-            </b-form-input>
-          </b-form-group>
-          <b-button type="submit" variant="primary">Next</b-button>
-          <b-button type="reset" variant="danger">Reset</b-button>
+        <b-form name="signUp" @submit="onSubmit" @reset="onReset">
+          <div v-show="step === 1">
+            <h2>Step 1</h2>
+            <b-form-group id="firstName"
+                          label="First Name:" class="text-left"
+                          label-for="firstNameLabel"
+                          description="Please Input Your First Name">
+              <b-form-input id="inputFirstName"
+                            type="text"
+                            v-model="userInfo.firstName"
+                            required
+                            placeholder="Input FirstName">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="lastName"
+                          label="Last Name:" class="text-left"
+                          label-for="lastNameLabel"
+                          description="Please Input Your Last Name">
+              <b-form-input id="inputLastName"
+                            type="text"
+                            v-model="userInfo.lastName"
+                            required
+                            placeholder="Input LastName">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="username"
+                          label="Username:" class="text-left"
+                          label-for="lastNameLabel"
+                          description="Please Input Your Username">
+              <b-form-input id="inputUsername"
+                            type="text"
+                            v-model="userInfo.username"
+                            required
+                            placeholder="Input Username">
+              </b-form-input>
+            </b-form-group>
+            <b-button type="reset" variant="danger">Reset</b-button>
+            <b-button @click="onNext" variant="primary">Next</b-button>
+          </div>
+          <div v-show="step === 2">
+            <h2>Step 2</h2>
+            <b-form-group id="email"
+                          label="Email Address:" class="text-left"
+                          label-for="emailAddressLabel"
+                          description="Please Input Your Email Address">
+              <b-form-input id="inputEmailAddress"
+                            type="email"
+                            v-model="userInfo.email"
+                            required
+                            placeholder="Input EmailAddress">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="password"
+                          label="Password:" class="text-left"
+                          label-for="passwordLabel"
+                          description="Please Input Your Password">
+              <b-form-input id="inputPassword"
+                            type="password"
+                            v-model="userInfo.password"
+                            required
+                            placeholder="Input Password">
+              </b-form-input>
+            </b-form-group>
+            <b-form-group id="ConfirmedPassword"
+                          label="Confirm Password:" class="text-left"
+                          label-for="lastNameLabel"
+                          description="Please Confirm Your Password">
+              <b-form-input id="inputConfirmedPassword"
+                            type="password"
+                            v-model="userInfo.confirmPassword"
+                            required
+                            placeholder="Input Confirmed Password">
+              </b-form-input>
+            </b-form-group>
+            <b-button type="reset" variant="danger">Reset</b-button>
+            <b-button @click="onPrevious" variant="info">Previous</b-button>
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </div>
         </b-form>
       </b-col>
       <b-col></b-col>
@@ -41,11 +93,14 @@ export default {
   name: 'sign-up',
   data() {
     return {
-      show: true,
       step: 1,
       userInfo: {
         firstName: '',
         lastName: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
       },
     };
   },
@@ -58,8 +113,23 @@ export default {
       evt.preventDefault();
       console.log(this);
       console.log(this.step);
-      this.userInfo.firstName = '';
-      this.userInfo.lastName = '';
+      if (this.step === 1) {
+        this.userInfo.firstName = '';
+        this.userInfo.lastName = '';
+        this.userInfo.username = '';
+      } else if (this.step === 2) {
+        this.userInfo.email = '';
+        this.userInfo.password = '';
+        this.userInfo.confirmPassword = '';
+      }
+    },
+    onNext(evt) {
+      evt.preventDefault();
+      this.step = this.step + 1;
+    },
+    onPrevious(evt) {
+      evt.preventDefault();
+      this.step = this.step - 1;
     },
   },
 };
