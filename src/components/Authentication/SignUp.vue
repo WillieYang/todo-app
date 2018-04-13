7bdd6533a520923c7e36df9f20ce40e1f60c6ff9
yangs <yangs@gisuni.com>
2018-04-13 10:33:51 +0800

    Add a sign up button wihch enables users to triggr the hidden modal using element ui dialog and steps

--- src/components/Authentication/SignUp.vue
+++ src/components/Authentication/SignUp.vue
@@ -3,127 +3,133 @@
     <b-row>
       <b-col></b-col>
       <b-col>
-        <b-form name="signUp" @submit="onSubmit" @reset="onReset">
-          <div v-if="step === 1">
-            <h2>{{ $t('step_1') }}</h2>
-            <b-form-group id="firstName"
-                          :label="$t('firstName')" class="text-left"
-                          label-for="firstNameLabel">
-              <b-form-input id="inputFirstName"
-                            type="text"
-                            v-model="userInfo.firstName"
-                            :placeholder="$t('firstName_input')"
-                            @change="$v.userInfo.firstName.$touch()">
-              </b-form-input>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.firstName.required && $v.userInfo.firstName.$dirty">
+        <el-button type="primary" @click="modalVisible = true">{{ $t('sign_up') }}</el-button>
+        <el-dialog :title="$t('sign_up')" :visible.sync="modalVisible" width="30%">
+          <el-steps :active="step" finish-status="success" class="no_center">
+            <el-step :title="$t('step_1')"></el-step>
+            <el-step :title="$t('step_2')"></el-step>
+          </el-steps>
+          <br>
+          <b-form name="signUp" @submit="onSubmit" @reset="onReset">
+            <div v-if="step === 1">
+              <b-form-group id="firstName"
+                            :label="$t('firstName')" class="text-left"
+                            label-for="firstNameLabel">
+                <b-form-input id="inputFirstName"
+                              type="text"
+                              v-model="userInfo.firstName"
+                              :placeholder="$t('firstName_input')"
+                              @change="$v.userInfo.firstName.$touch()">
+                </b-form-input>
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.firstName.required && $v.userInfo.firstName.$dirty">
                 {{ $t('firstName_required_msg') }}</pre>
-              <pre class="text-left text-danger"
-                    v-if="!$v.userInfo.firstName.minLength">
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.firstName.minLength">
                 {{ $t('firstName_error_msg',
                 {minLength: $v.userInfo.firstName.$params.minLength.min})}}
               </pre>
-            </b-form-group>
-            <b-form-group id="lastName"
-                          :label="$t('lastName')" class="text-left"
-                          label-for="lastNameLabel">
-              <b-form-input id="inputLastName"
-                            type="text"
-                            v-model="userInfo.lastName"
-                            :placeholder="$t('lastName_input')"
-                            @change="$v.userInfo.lastName.$touch()">
-              </b-form-input>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.lastName.required && $v.userInfo.lastName.$dirty">
+              </b-form-group>
+              <b-form-group id="lastName"
+                            :label="$t('lastName')" class="text-left"
+                            label-for="lastNameLabel">
+                <b-form-input id="inputLastName"
+                              type="text"
+                              v-model="userInfo.lastName"
+                              :placeholder="$t('lastName_input')"
+                              @change="$v.userInfo.lastName.$touch()">
+                </b-form-input>
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.lastName.required && $v.userInfo.lastName.$dirty">
                 {{ $t('lastName_required_msg') }}</pre>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.lastName.minLength">
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.lastName.minLength">
                 {{ $t('lastName_error_msg',
                 {minLength: $v.userInfo.lastName.$params.minLength.min})}}
               </pre>
-            </b-form-group>
-            <b-form-group id="username"
-                          :label="$t('username')" class="text-left"
-                          label-for="lastNameLabel">
-              <b-form-input id="inputUsername"
-                            type="text"
-                            v-model="userInfo.username"
-                            :placeholder="$t('username_input')"
-                            @change="$v.userInfo.username.$touch()">
-              </b-form-input>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.username.required && $v.userInfo.username.$dirty">
+              </b-form-group>
+              <b-form-group id="username"
+                            :label="$t('username')" class="text-left"
+                            label-for="lastNameLabel">
+                <b-form-input id="inputUsername"
+                              type="text"
+                              v-model="userInfo.username"
+                              :placeholder="$t('username_input')"
+                              @change="$v.userInfo.username.$touch()">
+                </b-form-input>
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.username.required && $v.userInfo.username.$dirty">
                 {{ $t('username_required_msg') }}</pre>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.username.minLength">
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.username.minLength">
                 {{ $t('username_error_msg',
                 {minLength: $v.userInfo.username.$params.minLength.min})}}
               </pre>
-            </b-form-group>
-            <b-button type="reset" variant="danger">{{ $t('reset_button') }}</b-button>
-            <b-button @click="onNext" variant="info"
-                      :disabled="$v.userInfo.firstName.$invalid
+              </b-form-group>
+              <b-button type="reset" variant="danger">{{ $t('reset_button') }}</b-button>
+              <b-button @click="onNext" variant="info"
+                        :disabled="$v.userInfo.firstName.$invalid
                       || $v.userInfo.lastName.$invalid
                       || $v.userInfo.username.$invalid">{{ $t('next_button') }}</b-button>
-          </div>
-          <div v-if="step === 2">
-            <h2>{{ $t('step_2') }}</h2>
-            <b-form-group id="email"
-                          :label="$t('email')" class="text-left"
-                          label-for="emailAddressLabel">
-              <b-form-input id="inputEmailAddress"
-                            type="email"
-                            v-model="userInfo.email"
-                            :placeholder="$t('email_input')"
-                            @change="$v.userInfo.email.$touch()">
-              </b-form-input>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.email.required && $v.userInfo.email.$dirty">
+            </div>
+            <div v-if="step === 2">
+              <b-form-group id="email"
+                            :label="$t('email')" class="text-left"
+                            label-for="emailAddressLabel">
+                <b-form-input id="inputEmailAddress"
+                              type="email"
+                              v-model="userInfo.email"
+                              :placeholder="$t('email_input')"
+                              @change="$v.userInfo.email.$touch()">
+                </b-form-input>
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.email.required && $v.userInfo.email.$dirty">
                 {{ $t('email_required_msg') }}</pre>
-              <pre class="text-left text-danger"
-                    v-if="!$v.userInfo.email.email">
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.email.email">
                 {{ $t('email_error_msg') }}
               </pre>
-            </b-form-group>
-            <b-form-group id="password"
-                          :label="$t('password')" class="text-left"
-                          label-for="passwordLabel">
-              <b-form-input id="inputPassword"
-                            type="password"
-                            v-model="userInfo.password"
-                            :placeholder="$t('password_input')"
-                            @change="$v.userInfo.password.$touch()">
-              </b-form-input>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.password.required && $v.userInfo.password.$dirty">
+              </b-form-group>
+              <b-form-group id="password"
+                            :label="$t('password')" class="text-left"
+                            label-for="passwordLabel">
+                <b-form-input id="inputPassword"
+                              type="password"
+                              v-model="userInfo.password"
+                              :placeholder="$t('password_input')"
+                              @change="$v.userInfo.password.$touch()">
+                </b-form-input>
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.password.required && $v.userInfo.password.$dirty">
                 {{ $t('password_required_msg') }}</pre>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.password.pwdLimit">
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.password.pwdLimit">
                 {{ $t('password_error_msg') }}
               </pre>
-            </b-form-group>
-            <b-form-group id="ConfirmedPassword"
-                          :label="$t('confirmedPwd')" class="text-left"
-                          label-for="lastNameLabel">
-              <b-form-input id="inputConfirmedPassword"
-                            type="password"
-                            v-model="userInfo.confirmPassword"
-                            :placeholder="$t('confirmedPwd_input')"
-                            @change="$v.userInfo.confirmPassword.$touch()">
-              </b-form-input>
-              <pre class="text-left text-danger"
-                   v-if="!$v.userInfo.confirmPassword.sameAsPassword
+              </b-form-group>
+              <b-form-group id="ConfirmedPassword"
+                            :label="$t('confirmedPwd')" class="text-left"
+                            label-for="lastNameLabel">
+                <b-form-input id="inputConfirmedPassword"
+                              type="password"
+                              v-model="userInfo.confirmPassword"
+                              :placeholder="$t('confirmedPwd_input')"
+                              @change="$v.userInfo.confirmPassword.$touch()">
+                </b-form-input>
+                <pre class="text-left text-danger"
+                     v-if="!$v.userInfo.confirmPassword.sameAsPassword
                    && $v.userInfo.confirmPassword.$dirty">
                 {{ $t('confirmedPwd_error_msg') }}
               </pre>
-            </b-form-group>
-            <b-button type="reset" variant="danger">{{ $t('reset_button') }}</b-button>
-            <b-button @click="onPrevious" variant="info">{{ $t('previous_button') }}</b-button>
-            <b-button type="submit" variant="primary" :disabled="$v.$invalid">
-              {{ $t('submit_button') }}
-            </b-button>
-          </div>
-        </b-form>
+              </b-form-group>
+              <b-button type="reset" variant="danger">{{ $t('reset_button') }}</b-button>
+              <b-button @click="onPrevious" variant="info">{{ $t('previous_button') }}</b-button>
+              <b-button type="submit" variant="primary" :disabled="$v.$invalid">
+                {{ $t('submit_button') }}
+              </b-button>
+            </div>
+          </b-form>
+        </el-dialog>
       </b-col>
       <b-col></b-col>
     </b-row>
@@ -148,6 +154,7 @@ export default {
         password: '',
         confirmPassword: '',
       },
+      modalVisible: false,
     };
   },
   validations: {
@@ -207,13 +214,13 @@ export default {
   },
 };
 </script>
-  pre {
-text-align: left;
-white-space: pre-line;
-}
+
 <style scoped>
 pre {
   text-align: left;
   white-space: pre-line;
+}
+.no_center {
+  text-align: justify;
 }
 </style>
