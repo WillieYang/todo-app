5bf6a26d7ca4d7e207796772e34bcb2e1bfd2725
yangs <yangs@gisuni.com>
2018-06-13 16:33:54 +0800

    Integrate the socket.io with a component in a vue project

--- src/components/socketIO/SocketChat.vue
+++ src/components/socketIO/SocketChat.vue
@@ -1,12 +1,45 @@
 <template>
-    <div>
-      <h1> Hello World </h1>
+    <div class="card mt-3">
+      <div class="card-body">
+        <div class="card-title">
+          <h3>Chat Group</h3>
+          <hr>
+        </div>
+        <div class="card-body">
+          <div class="messages"></div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <b-form @submit.prevent="sendmessage">
+          <b-form-group class="form-group"
+                        label-for="user"
+                        label="User:">
+            <b-form-input type="text" v-model="user" class="form-control"></b-form-input>
+          </b-form-group>
+          <b-form-group class="form-group"
+                        label-for="message"
+                        label="Message:">
+            <b-form-input type="text" v-model="message" class="form-control"></b-form-input>
+          </b-form-group>
+          <b-button type="submit" variant="success">Send</b-button>
+        </b-form>
+      </div>
     </div>
 </template>
 
 <script>
+import io from 'socket.io-client';
+
 export default {
   name: 'socket-chat',
+  data() {
+    return {
+      user: '',
+      message: '',
+      messages: [],
+      socket: io('localhost:3001'),
+    };
+  },
 };
 </script>
 
