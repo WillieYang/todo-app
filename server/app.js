300d283d44882cb64ebaa1cca9ed8c28384fe9c1
yangs <yangs@gisuni.com>
2018-06-13 15:09:43 +0800

    Installation of socket.io-client in vue project

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
-// /* global server:true */
-// /* eslint no-undef: "error" */
-//
-
 const express = require('express');
 
 const app = express();
 
-server = app.listen(3001, function () {
+const server = app.listen(3001, function () {
   console.log('server is runnig on port 3001');
+});
+
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
 });
