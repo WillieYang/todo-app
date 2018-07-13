fe9747ddeafec1de21c8d21b178483f9405adf34
yangs <yangs@gisuni.com>
2018-07-13 09:46:19 +0800

    use gulp to add a selector namespace for theme black

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp')
 var cleanCSS = require('gulp-clean-css')
 var cssWrap = require('gulp-css-wrap')
 gulp.task('css-wrap', function () {
-  return gulp.src(path.resolve('./theme/red/index.css'))
+  return gulp.src(path.resolve('./theme/black/index.css'))
   /* 找需要添加命名空间的css文件，支持正则表达式 */
   .pipe(cssWrap({
-    selector: '.theme-red' /* 添加的命名空间 */
+    selector: '.theme-black' /* 添加的命名空间 */
   }))
   .pipe(cleanCSS())
-  .pipe(gulp.dest('src/assets/css/theme/red')) /* 存放的目录 */
+  .pipe(gulp.dest('src/assets/css/theme/black')) /* 存放的目录 */
 })
